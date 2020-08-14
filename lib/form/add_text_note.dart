38d55dc1a5a1ad62343cc85ed77c120b06fc1ef3
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-14 18:25:11 +0545

    Add form fields and a save button

--- lib/form/add_text_note.dart
+++ lib/form/add_text_note.dart
@@ -17,6 +17,49 @@ class _AddTextNoteState extends State<AddTextNote> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(title: new Text('Add Text Note')),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+          child: ListView(
+            children: <Widget>[
+              Theme(
+                data: ThemeData(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  inputDecorationTheme:
+                      InputDecorationTheme(border: InputBorder.none),
+                ),
+                child: Form(
+                  key: _addTextNoteFormKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 50),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: TextFormField(
+                          keyboardType: TextInputType.multiline,
+                          minLines: 1,
+                          maxLines: 2,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            hintText: 'What do you want to write about?',
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        decoration: InputDecoration(hintText: 'Tell me more.'),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _handleAddTextNote();

41831f970a2277af31b754d76f99e4389b165688
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-16 12:06:19 +0545

    Added db helper class

---
+++ lib/db/db_helper.dart
@@ -0,0 +1,8 @@
+class DBHelper {
+  String table = 'diary_data';
+  String id = 'id';
+  String title = 'title';
+  String description = 'description';
+  String type = 'type';
+  String date = 'date';
+}

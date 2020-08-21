99cc61f634acd8a7f98a3bc73fc6444da683bda5
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-21 19:45:04 +0545

    Add content to individual note detail

--- lib/view/note_detail.dart
+++ lib/view/note_detail.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_diary/model/note.dart';
 
 class NoteDetail extends StatelessWidget {
+  final Note note;
+
+  NoteDetail({this.note});
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return (Scaffold(
+      appBar: AppBar(title: Text('Note Details')),
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Title: ' + note.title, style: TextStyle(fontSize: 22)),
+            Text('Description: ' + note.description,
+                style: TextStyle(fontSize: 22)),
+            Text('Date: ' + note.date, style: TextStyle(fontSize: 22)),
+          ],
+        ),
+      ),
+    ));
   }
 }
